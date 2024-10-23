800b9c9c56d6d92a44f6255b31adc8ae8e9b21cb
maisaachaves <159539044+maisaachaves@users.noreply.github.com>
2024-10-23 08:41:31 -0300

    kk

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'história',
+    'Em qual período da pré-historia o fogo foi descoberto?',
+    'paleolítico',
 )
 
 criaCartao(
     categoria,
-    pergunta,
-    resposta
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Tem entre 4 a 6 litros. São retirados 450 mililitros',
 )
 
 criaCartao(
